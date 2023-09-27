d9f4fb61b2ffd4a9e2fa620cd4121ecf2d6781b7
Ryan Heuser <ryan.heuser@princeton.edu>
2023-09-27 17:59:07 -0400

    work to merge everything

--- ppanlp/imports.py
+++ ppanlp/imports.py
@@ -6,7 +6,24 @@ from tqdm import tqdm
 from sqlitedict import SqliteDict
 import orjson,zlib
 import pandas as pd
+from intspan import intspan
 pd.options.display.max_columns=None
 
-path_ecco_pages_cache = os.path.expanduser('~/data/ecco/ecco_pages.sqlitedict')
-path_root_eccoII = os.path.expanduser('~/data/ecco/ECCOII')
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+PATH_REPO = os.path.dirname(PATH_HERE)
+PATH_REPO_DATA = os.path.join(PATH_REPO, 'data')
+PATH_HOME_DATA = os.path.expanduser('~/ppa_data')
+PATH_ECCO_DATA = os.path.join(PATH_HOME_DATA, 'ecco')
+PATH_ECCO_RAW_DATA = os.path.join(PATH_ECCO_DATA, 'raw')
+PATH_CORPUS_RAW_ECCO1=os.path.join(PATH_ECCO_RAW_DATA, 'ECCO1')
+PATH_CORPUS_RAW_ECCO2=os.path.join(PATH_ECCO_RAW_DATA, 'ECCO2')
+PATH_ECCO_PAGES_CACHE = os.path.join(PATH_ECCO_DATA,'ecco_pages.sqlitedict')
+PATH_ECCO_EXCERPTS = os.path.join(PATH_REPO_DATA,'ECCO Excerpts-DigitizedWork-2023-09-20.csv')
+PATHS_METADATA = [
+    os.path.join(PATH_REPO_DATA, 'data.all_xml_metadata.ECCO1.csv'), 
+    os.path.join(PATH_REPO_DATA, 'data.all_xml_metadata.ECCO2.csv')
+]
+
+
+for pathstr in [PATH_HOME_DATA, PATH_ECCO_DATA, PATH_ECCO_RAW_DATA]:
+    os.makedirs(pathstr, exist_ok=True)
--- ppanlp/ppanlp.py
+++ ppanlp/ppanlp.py
@@ -3,7 +3,7 @@ from .imports import *
 
 def encode_cache(x): return zlib.compress(orjson.dumps(x))
 def decode_cache(x): return orjson.loads(zlib.decompress(x))
-def get_page_cache(fn=path_ecco_pages_cache):
+def get_page_cache(fn=PATH_ECCO_PAGES_CACHE):
     return SqliteDict(fn, autocommit=True, encode=encode_cache, decode=decode_cache)
 
 def get_pages(text_id, page_id=None):
@@ -14,10 +14,52 @@ def get_pages(text_id, page_id=None):
         return {}
     
 @cache
-def get_metadata():
-    files_root_eccoII = os.listdir(path_root_eccoII)
-    excel_file_paths = [os.path.join(path_root_eccoII,fn) for fn in files_root_eccoII if fn.endswith('.xlsx')]
-    return pd.concat(
-        pd.read_excel(path).assign(subcorpus=os.path.splitext(os.path.basename(path))[0])
-        for path in tqdm(excel_file_paths)
-    )
+def get_ecco_metadata(fns = PATHS_METADATA):
+    l = [pd.read_csv(fn) for fn in fns]
+    df = pd.concat(l) if l else pd.DataFrame()
+    assert len(df) and 'productlink' in set(df.columns)
+    df['source_id'] = df['productlink'].apply(lambda x: x.split('GALE%7C')[1])
+    return df.set_index('source_id')
+
+@cache
+def get_ecco_excerpts():
+    return pd.read_csv(PATH_ECCO_EXCERPTS).set_index('source_id')
+
+@cache
+def get_ecco_pages():
+    # Function to get pages for text, using sqlitedict cache made from ECCO page XMLs
+
+    with get_page_cache() as cache:
+        def get_pages_row(row):
+            textid=row.psmid
+            textpages = cache.get(textid)
+            if not textpages: return []
+            
+            opages = []
+            pagerange=row.pages_digital
+            if not pagerange: 
+                opages = list(textpages.values())
+            else:
+                for page_num in intspan(pagerange):
+                    page_id = f'{page_num:04}0'
+                    page = textpages.get(page_id)
+                    if page:
+                        opages.append(page)
+            
+            for d in opages:
+                d['source_id']=row.source_id
+
+            return opages
+        df_excerpts = get_ecco_excerpts()
+        df_meta = get_ecco_metadata()
+        df_merged = df_excerpts.merge(df_meta, on='source_id').reset_index()
+        all_pages = [page for i,row in tqdm(df_merged.iterrows(), total=len(df_merged)) for page in get_pages_row(row)]
+        pages_df = pd.DataFrame(all_pages, dtype=str).set_index('source_id')
+        return pages_df
+    
+@cache
+def get_all_ecco_data_for_excerpts():
+    df_excerpts = get_ecco_excerpts()
+    df_meta = get_ecco_metadata()
+    df_pages = get_ecco_pages()
+    return df_pages.merge(df_excerpts, on='source_id').merge(df_meta, on='source_id')
