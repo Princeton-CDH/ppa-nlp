d452c6961d01e5c88f098908d1184805b895208b
Ryan Heuser <ryan.heuser@princeton.edu>
2023-12-10 13:28:17 -0500

    setup py fix

--- ppanlp/corpus.py
+++ ppanlp/corpus.py
@@ -314,7 +314,7 @@ class PPACorpus:
                 if num_proc is None: 
                     num_proc=mp.cpu_count() - 1 if mp.cpu_count()>1 else 1
                 with logwatch(f'saving jsonl files to {self.path_texts_preproc} [{num_proc}x]') as lw:
-                    for d in self.iter_pages_jsonl(as_dict=True, desc=f"sending each text's pages to an {num_proc} CPU multiprocessing pool"):
+                    for d in self.iter_pages_jsonl(as_dict=True, desc=f"sending each text's pages to a multiprocessing pool with {num_proc} CPUs"):
                         work_id=d.get('work_id')
                         wdb[work_id].add(d['page_id'])
                         if last_pages and work_id!=last_work_id:
