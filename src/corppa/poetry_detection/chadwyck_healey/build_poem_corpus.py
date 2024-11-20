1bb80b40e2a70db977bbe214b961cdd5c1666036
Laure Thompson <602628+laurejt@users.noreply.github.com>
2024-11-20 11:12:15 -0500

    In-progress effort for parsing Chadwyck Healey

--- src/corppa/poetry_detection/chadwyck_healey/build_poem_corpus.py
+++ src/corppa/poetry_detection/chadwyck_healey/build_poem_corpus.py
@@ -6,27 +6,32 @@ import argparse
 import pathlib
 import re
 import sys
+from collections import Counter
+from typing import Any
 
 import bs4
 import ftfy
 import orjsonl
 from tqdm import tqdm
 
-_symbol_table = {
-    "&indent;": "\t",
-    "&wblank;": "\u2014\u2014",
-    "&lblank;": "\u2014",
-    "&grwc;": "\u1ff6",
-    "&grst;": "\u03c2",
+# TODO: Revisit this exclusion list
+EXCLUDED_POEMS = {
+    "Z200677556",  # Weird poem constructed out of out of unordered lists
 }
-
-
-def clean_text(text):
-    new_text = ftfy.fix_text(text)
-    # Complete symbol replacement
-    for symbol_str, out_str in _symbol_table.items():
-        new_text = new_text.replace(symbol_str, out_str)
-    return new_text.rstrip()
+EXCLUDED_EDITIONS = {
+    "Z000594240",  # Modern poetry, poem made of unordered lists
+    "Z000359125",  # Modern poetry, poem simply containing an image
+    "Z000361067",  # Modern poetry, poem composed of images & captions
+    "Z000228617",  # Modern poetry, "poem" that's just a note
+    "Z000133831",  # Modern potery, shaped poem uses sl tags
+    "Z000683334",  # Modern poetry, poem made up of empty lines
+    "Z000582255",  # Modern poetry, poem made of unordered lists
+    "Z000229980",  # Modern poetry, poem w/ malformed unordered lists
+    "Z000303274",  # Modern poetry, poem w/ malformed unordered lists
+    "Z000306246",  # Modern poetry, poem w/ malformed unordered lists
+    "Z000605113",  # Modern poetry, poem made of unordered list
+    "Z000604899",  # Modern poetry, poem that's a figure
+}
 
 
 # Should this be moved to path utils?
@@ -44,91 +49,36 @@ def get_poem_subdir(poem_id):
     return f"{match.groups()[0]}{match.groups()[1][::3]}"
 
 
-def extract_tag_data(tag_soup):
-    tag_info = {}
-    # Attribute handling
-    for attr, val in tag_soup.attrs.items():
-        tag_info[f"{tag_soup.name}_{attr}"] = val
-
-    # Children tag handling
-    for child_tag in tag_soup.find_all(True, recursive=False):
-        # Assume child tag has no children tags
-        assert child_tag.find(True, recursive=False) is None
-        # Handle child tag attributes
-        for attr, val in child_tag.attrs.items():
-            field_name = f"{child_tag.name}_{attr}"
-            assert field_name not in tag_info
-            tag_info[field_name] = val
-        # Handle child's text
-        assert child_tag.name not in tag_info
-        tag_info[child_tag.name] = clean_text(child_tag.text)
-    return tag_info
-
-
-def extract_metadata(filename):
-    metadata = {}
-    with open(filename, encoding="latin1") as reader:
-        raw_text = reader.read()
-    # This will only parse the "head" block, but won't
-    # modify the meta tag
-    soup = bs4.BeautifulSoup(raw_text, "lxml-xml")
-    for tag in soup.head.find_all(True, recursive=False):
-        assert f"{tag.name}_meta" not in metadata
-        metadata[f"{tag.name}_meta"] = extract_tag_data(tag)
-    return metadata
-
-
-def get_span_text(span_tag):
-    span_text = ""
-    # Get span text
-    for child in span_tag.children:
-        # Children are either NavigableStrings or Tags
-        if isinstance(child, bs4.NavigableString):
-            span_text += child.text
-        elif child.name == "span":
-            span_text += get_span_text(child)
-        else:
-            raise ValueError(f"Unexpected span child tag '{child.name}'")
-    # TODO: Set span styling (if this seems at all valuable)
-    return span_text
-
-
-def get_div_text(div_tag):
-    if div_tag.name != "div":
-        raise ValueError(f"{div_tag} is not a div tag")
-    div_type = div_tag["type"]
-    # Handle each div type
-    if div_type in ["versepara", "stanza"]:
-        # Case #1: Group of text (should be separated by whitespace)
-        text_block = ""
-        # Should contain a group of lines
-        for child_div in div_tag.find_all(True, recursive=False):
-            assert child_div.name == "div"
-            text_block += get_div_text(child_div)
-        return text_block + "\n"
-    elif div_type in ["line", "firstline", "conclusion", "greek"]:
-        # Case #2: Some text
-        # There might be weird leading whitespace if a note was skipped
-        line = ""
-        for child in div_tag.children:
-            # Children are either NavigableStrings or Tags
-            if isinstance(child, bs4.NavigableString):
-                # attempt to remove singleton whitespace
-                if child.text == " ":
-                    line += ""
-                else:
-                    line += child.text
-            elif child.name == "div":
-                line += get_div_text(child)
-            elif child.name == "span":
-                line += get_span_text(child)
-            elif child.name == "edit":
-                # Skip editorial tags
-                line += ""
-            else:
-                raise ValueError(f"Unexpected div child tag '{child.name}'")
-        return line + "\n"
-    elif div_type in [
+class TMLParser:
+    SYMBOL_TABLE = {
+        "&indent;": "\t",
+        "&wblank;": "\u2014\u2014",
+        "&lblank;": "\u2014",
+        "&grwc;": "\u1ff6",
+        "&grst;": "\u03c2",
+    }
+    TEXT_BLOCKS = {"span", "p"}
+    STYLE_TAGS = {"sub", "sup", "it", "u"}
+    SKIPPED_TAGS = {
+        "edit",
+        "stage",
+        "break",
+        "epilogue",
+        "img",
+        "castlist",
+        "ul",
+        "ul2",
+    }
+    WORKING_DIV_TYPES = {
+        "versepara",
+        "stanza",
+        "line",
+        "firstline",
+        "conclusion",
+        "greek",
+        "speaker",
+    }
+    SKIPPED_DIV_TYPES = {
         "argument",
         "caption",
         "dedication",
@@ -137,50 +87,219 @@ def get_div_text(div_tag):
         "preface",
         "signed",
         "trailer",
-    ]:
-        # Skip these blocks
-        return ""
-    else:
-        print(f"Skipping unexpected div type '{div_type}'", file=sys.stderr)
-        return ""
+        "copyright",  # revisit
+        "prologue",  # revisit
+    }
 
+    def __init__(self, file: pathlib.Path) -> None:
+        self.filepath = str(file)
+        self.filename = file.stem
+        self.log_count = 0
+        self.parse_tml(file)
 
-def extract_text(filename):
-    with open(filename, encoding="latin1") as reader:
-        raw_text = reader.read()
-    # Care about body block
-    soup = bs4.BeautifulSoup(raw_text, "lxml").body
-    text = ""
-    if soup.find("div", recursive=False) is None:
-        # Attempted workaround for TEI
-        tag_depth = sum(1 for _ in soup.parents) + 1
-        for div in soup.find_all("div"):
-            parent_names = [p.name for p in div.parents][::-tag_depth]
-            # Skip nested divs
-            if "div" in parent_names:
+    def __str__(self) -> str:
+        return self.filepath
+
+    def print_log(self, message):
+        log_message = f"{self}: {message}"
+        if self.log_count:
+            print(log_message)
+        else:
+            # tqdm workaround
+            print("\n" + log_message)
+        self.log_count += 1
+
+    def parse_tml(self, file: pathlib.Path):
+        """
+        Parse TML file and extract head and body tags
+        """
+        # 1. Read in raw text
+        with open(file, encoding="latin1") as reader:
+            text = reader.read()
+
+        # 2. Preprocess text
+        ## Hack to fix meta tag for processing header
+        text = text.replace("<meta>", "<metadata")
+        text = text.replace("</meta>", "</metadata")
+        ## Hack attempt to fix ul2/list2 issue
+        # if "<list2>" not in text and "<ul2>" in text:
+        #    assert "</ul2>" not in text
+        #    text = text.replace("</list2>", "</ul2>")
+        # TODO: Also deal with mismatched
+
+        ## Replace known problematic tags for processing content
+        tag_substitutions = [
+            ("figure", r"<figure[^>]*>", ""),
+            ("gap", r"<gap>", " "),
+            ("div3", r"<div3[^>]*>", ""),
+            ("div4", r"<div4[^>]*>", ""),
+            ("div5", r"<div5[^>]*>", ""),
+            ("caesura", r"<caesura>", ""),
+            ("blnkpage", r"<blnkpage>", ""),
+            ("xref", r"<xref[^>]*>", ""),
+        ]
+        for tag, pattern, repl in tag_substitutions:
+            assert r"</{tag}>" not in text
+            text, n_subs = re.subn(pattern, repl, text)
+        if n_subs:
+            self.print_log(f"Deleted {n_subs} {tag} tags")
+
+        # 3. Run through Beautiful Soup
+        soup = bs4.BeautifulSoup(text, "lxml")
+        self.head = soup.head
+        self.body = soup.body
+
+    def clean_text(self, text: str) -> str:
+        new_text = ftfy.fix_text(text)
+        # Complete symbol replacement
+        for symbol_str, out_str in self.SYMBOL_TABLE.items():
+            new_text = new_text.replace(symbol_str, out_str)
+        return new_text.rstrip()
+
+    def _extract_tag_data(self, tag_soup: bs4.Tag) -> dict[str, str]:
+        tag_info = {}
+        # Attribute handling
+        for attr, val in tag_soup.attrs.items():
+            tag_info[f"{tag_soup.name}_{attr}"] = val
+
+        # Children tag handling
+        for child_tag in tag_soup.find_all(True, recursive=False):
+            # Assume child tag has no children tags
+            assert child_tag.find(True, recursive=False) is None
+            # Handle child tag attributes
+            for attr, val in child_tag.attrs.items():
+                field_name = f"{child_tag.name}_{attr}"
+                assert field_name not in tag_info
+                tag_info[field_name] = val
+            # Handle child's text
+            assert child_tag.name not in tag_info
+            tag_info[child_tag.name] = self.clean_text(child_tag.text)
+        return tag_info
+
+    def extract_metadata(self) -> dict[str, dict[str, str]]:
+        """
+        Extract meta data from head tag.
+        """
+        metadata = {}
+        tag_types = Counter(
+            [tag.name for tag in self.head.find_all(True, recursive=False)]
+        )
+        for tag in self.head.find_all(True, recursive=False):
+            if tag.name == "body":
+                # This shouldn't happen, unless there's a formatting issue
+                self.print_log(f"Encountered body tag in head tag, skipping")
                 continue
-            # Skip divs within edit tags
-            if "edit" in parent_names:
-                continue
-            text += get_div_text(div)
-    for div in soup.find_all("div", recursive=False):
-        # Skip nested div tags
-        text += get_div_text(div)
-    return clean_text(text)  # Remove trailing whitespace
+            working_tag = f"{tag.name}"
+            if tag_types[tag.name] > 1:
+                # Differentiate duplicate tab types by their id
+                if "id" in tag.attrs:
+                    working_tag = f"{tag.name}_{tag['id']}"
+            elif tag.name == "metadata":
+                working_tag = "meta"  # Correct for meta tag hack
+            if working_tag in metadata:
+                raise ValueError(f"{self}: Duplicate metadata tags")
+            metadata[working_tag] = self._extract_tag_data(tag)
+        return metadata
+
+    def get_bs4_text(self, tag_or_string=bs4.NavigableString | bs4.Tag) -> str:
+        """
+        Extract text from a BeautifulSoup Tag or NavigableString
+        """
+        if isinstance(tag_or_string, bs4.NavigableString):
+            return str(tag_or_string)
+        else:
+            tag = tag_or_string
+            if tag.name == "div":
+                return self.get_div_text(tag)
+            elif tag.name in self.SKIPPED_TAGS:
+                # Skip these tags
+                return ""
+            elif tag.name in self.TEXT_BLOCKS | self.STYLE_TAGS:
+                # In these cases, simply accumulate children's texts
+                text = ""
+                for child in tag.children:
+                    text += self.get_bs4_text(child)
+                if child.name == "p":
+                    # For paragraph tag, add ending newline
+                    return text + "\n"
+                return text
+            else:
+                # Fallback: Skip and log doing so
+                self.print_log(f"Skipping unexpected tag type '{tag.name}'")
+                return ""
+
+    def get_div_text(self, div_tag: bs4.Tag) -> str:
+        """
+        Extract text from a div tag.
+        """
+        if div_tag.name != "div":
+            raise ValueError(f"{div_tag} is not a div tag")
+        div_type = div_tag["type"]
+        # Handle each div type
+        if div_type in self.WORKING_DIV_TYPES:
+            text = ""
+            for child in div_tag.children:
+                text += self.get_bs4_text(child)
+            return text + "\n"
+        elif div_type in self.SKIPPED_DIV_TYPES:
+            # Case 3: Skippable div tags
+            return ""
+        else:
+            # Fallback: Skip and log doing so
+            self.print_log(f"Skipping unexpected div type '{div_type}'")
+            return ""
+
+    def extract_text(self):
+        """
+        Extract text from body tag
+        """
+        text = ""
+        #    if soup.find("div", recursive=False) is None:
+        #        # Attempted workaround for TEI
+        #        tag_depth = sum(1 for _ in soup.parents) + 1
+        #        for div in soup.find_all("div"):
+        #            parent_names = [p.name for p in div.parents][::-tag_depth]
+        #            # Skip nested divs
+        #            if "div" in parent_names:
+        #                continue
+        #            # Skip divs within edit tags
+        #            if "edit" in parent_names:
+        #                continue
+        #            text += get_div_text(div, message_pfx=filename)
+        # Assume core text is in divs
+        for div in self.body.find_all("div", recursive=False):
+            text += self.get_div_text(div)
+        # If no text found, attempt to use p tags for content
+        if not text:
+            self.print_log("Attempting to use p tags")
+            for p_tag in self.body.find_all("p", recursive=False):
+                text += self.get_bs4_text(p_tag)
+
+        # Clean resulting text
+        final_text = self.clean_text(text)
+        # Fail if no text extracted
+        assert final_text, f"No text extracted from {self}"
+        return final_text
 
 
-def get_poem_record(poem_fn):
-    poem_metadata = extract_metadata(poem_fn)
+def get_poem_record(poem_fn: pathlib.Path, skip_editions: set[str] = EXCLUDED_EDITIONS):
+    poem_parser = TMLParser(poem_fn)
+    # Extract Metadata
+    poem_metadata = poem_parser.extract_metadata()
     record = {}
-    record["poem_id"] = poem_metadata["title_meta"]["title_id"]
+    record["poem_id"] = poem_metadata["title"]["title_id"]
     record |= poem_metadata
-
-    try:
-        poem_text = extract_text(poem_fn)
-    except (ValueError, NotImplementedError) as err:
-        raise type(err)(f"{poem_fn.stem}: {err}")
-
-    record["text"] = poem_text
+    # Check if poem should be skipped based on its edition
+    if skip_editions:
+        edition_id = poem_metadata["title"]["edition_id"]
+        if edition_id in skip_editions:
+            return None
+    # Check if text was extracted succesffully
+    extracted_text = poem_parser.extract_text()
+    if extracted_text is None:
+        # Encountered failure
+        return None
+    record["text"] = extracted_text
     return record
 
 
@@ -189,7 +308,7 @@ def get_poem_records(in_dir, poem_ids=None, show_progress=True):
     desc = "Extracting poems"
     if poem_ids:
         # Gather selected poem records
-        poem_set = set(poem_ids)
+        poem_set = set(poem_ids) - EXCLUDED_POEMS
         for poem_id in tqdm(
             poem_set, desc=desc, total=len(poem_set), disable=disable_progress
         ):
@@ -198,7 +317,11 @@ def get_poem_records(in_dir, poem_ids=None, show_progress=True):
             if not poem_path.is_file():
                 print(f"Warning: poem file {poem_path} does not exist")
                 continue
-            yield get_poem_record(poem_path)
+            result = get_poem_record(poem_path)
+            if result:
+                yield get_poem_record(poem_path)
+            # else:
+            #    print(f"Warning: skipped {poem_path}")
     else:
         # Gather all poem records
         bar_format = "{desc}: {n:,} poems extracted | elapsed: {elapsed}, {rate_fmt}"
@@ -206,9 +329,16 @@ def get_poem_records(in_dir, poem_ids=None, show_progress=True):
             in_dir.rglob("*.tml"),
             desc=desc,
             bar_format=bar_format,
+            dynamic_ncols=True,
             disable=disable_progress,
         ):
-            yield get_poem_record(poem_path)
+            if poem_path.stem in EXCLUDED_POEMS:
+                continue
+            result = get_poem_record(poem_path)
+            if result:
+                yield result
+            # else:
+            #    print(f"Warning: skipped {poem_path}")
 
 
 def save_poem_records(in_dir, out_fn, poem_ids=None, show_progress=True):
@@ -227,7 +357,7 @@ def main():
     )
     parser.add_argument(
         "output",
-        help="FIlename where the (sub)corpus should be saved",
+        help="Filename where the (sub)corpus should be saved",
         type=pathlib.Path,
     )
     # Optional
@@ -238,9 +368,9 @@ def main():
         default=True,
     )
     parser.add_argument(
-        "--poems",
+        "--poem-ids",
         help="Create subcorpus limited to the specified poems (by id)."
-        "Can be repeated/",
+        "Can be repeated.",
         nargs="*",
         action="extend",
     )
@@ -256,17 +386,17 @@ def main():
     if not output_fn.suffix:
         output_fn = args.output.with_suffix(".jsonl")
     # Check that output file does not exist
-    if output_fn.is_file():
+    if output_fn.exists():
         print(f"Error: output file {args.output} already exists", file=sys.stderr)
         sys.exit(1)
 
     try:
         save_poem_records(
-            args.input, args.output, poem_ids=args.poems, show_progress=args.progress
+            args.input, output_fn, poem_ids=args.poem_ids, show_progress=args.progress
         )
     except Exception as err:
         # Remove output if it exists
-        args.output.unlink(missing_ok=True)
+        # args.output.unlink(missing_ok=True)
         # Rethrow error
         raise err
 
