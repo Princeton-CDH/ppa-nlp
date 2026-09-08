16eefd4bdbda5da33d8de9d0802a54583dde26d2
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-09-08 16:02:15 -0400

    Basic notebook to review hathitrust page alignment locally

---
+++ notebooks/ppa-alignment/review.py
@@ -0,0 +1,51 @@
+import marimo
+
+__generated_with = "0.23.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    from pathlib import Path
+
+    import polars as pl
+
+    from corppa.utils.dataset_prep import (
+        get_ht_zipfile_path,
+        plot_alignment,
+        review_alignment,
+    )
+
+    work_id = "hvd.hn1dlr"
+    # work_id = "hvd.32044048962955-p145"
+    # work_id = "hvd.32044050831999-p50"
+    # work_id = "hvd.32044092711431-p469"
+
+    data_dir = Path("/Users/rkoeser/workarea/experiments/ppa-textimage-alignment")
+
+    all_pages_path = Path(
+        "~/workarea/experiments/ppa-textimage-alignment/ppa_corpus_2026-01-07_091133/ppa_pages.jsonl"
+    ).expanduser()
+    print(all_pages_path)
+    # load original pages
+    orig_pages_df = (
+        pl.scan_ndjson(all_pages_path).filter(work_id=work_id).collect().sort("order")
+    )
+    orig_pages_df
+
+    zip_path = get_ht_zipfile_path(work_id, data_dir)
+    df = review_alignment(
+        work_id, orig_pages_df, zip_path
+    )  # pages: list[dict] or DataFrame
+    plot_alignment(df)
+    return df, pl
+
+
+@app.cell
+def _(df, pl):
+    df.filter(~pl.col.is_matched)  # inspect gaps
+    return
+
+
+if __name__ == "__main__":
+    app.run()
